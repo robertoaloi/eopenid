80f1f712d88727475703df861b800f0435bbbcff
Roberto Aloi <prof3ta@lestat-desktop.site>
2009-11-23 18:49:09 +0000

    Fixed eopenid.erl interface to match EW specifications.

--- src/eopenid.erl
+++ src/eopenid.erl
@@ -4,25 +4,26 @@
 %%%
 %%% Created   :  3 Apr 2008 by Torbjorn Tornkvist <tobbe@tornkvist.org>
 %%% Re-worked :  7 Feb 2009 by Torbjorn Tornkvist <tobbe@tornkvist.org>
-%%% Desc.     :  Implementation of OpenID v1.1 
+%%% Desc.     :  Implementation of OpenID v1.1
 %%%-------------------------------------------------------------------
 -module(eopenid).
 
-
--export([start/0, start/1, stop/0]).
+-export([install/1, uninstall/1, dependencies/0]).
 
 -ignore_xref([{start,0},{start,1},{stop,0}]).
 
-start() ->
+install(Config) ->
     application:start(inets),
     application:start(crypto),
-    application:start(eopenid).
+    case eopenid_sup:start_link(Config) of
+	{ok, Pid} ->
+	    {ok, Pid};
+	Error ->
+	    Error
+    end.
 
-start(Type) ->
-    application:start(inets),
-    application:start(crypto),
-    application:start(eopenid, Type).
+uninstall([]) ->
+    ok.
 
-
-stop() ->
-    application:stop(eopenid).
+dependencies() ->
+    [{e_auth, {1.0, undefined}}].
